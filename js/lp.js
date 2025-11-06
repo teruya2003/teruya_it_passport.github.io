4c883db3f2153ea9b6aa7c7d6396eaeed5cb7aa2
teruya2003 <teruyahirahara@gmail.com>
2025-11-06 13:55:31 +0900

    LPの完成

--- js/lp.js
+++ js/lp.js
@@ -190,8 +190,8 @@
   // スクロールアニメーション
   // ============================================
   const observerOptions = {
-    threshold: 0.15,
-    rootMargin: '0px 0px -100px 0px'
+    threshold: 0.05,
+    rootMargin: '0px 0px -50px 0px'
   };
 
   const observer = new IntersectionObserver(function(entries) {
